079eb6fc8d80a46e9f57fb6ca88132e0bab9f770
Tiep Bui <tiep.bui@jmango360.com>
2021-05-23 18:42:44 +0700

    Empty state added

---
+++ pages/_app.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import App from 'next/app';
+import Head from 'next/head';
+import { AppProvider } from '@shopify/polaris';
+import '@shopify/polaris/dist/styles.css';
+import translations from '@shopify/polaris/locales/en.json';
+class MyApp extends App {
+  render() {
+    const { Component, pageProps } = this.props;
+
+    return (
+      <React.Fragment>
+        <Head>
+          <title>Sample App</title>
+          <meta charSet="utf-8" />
+        </Head>
+          <AppProvider i18n={translations}>
+            <Component {...pageProps} />
+          </AppProvider>
+      </React.Fragment>
+    );
+  }
+}
+
+// MyApp.getInitialProps = async ({ ctx }) => {
+//   return {
+//     shopOrigin: ctx.query.shop,
+//   }
+// }
+
+export default MyApp;
