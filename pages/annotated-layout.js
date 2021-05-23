839a110095c357b1b95b26495d313990c7ebf14c
Tiep Bui <tiep.bui@jmango360.com>
2021-05-23 18:45:41 +0700

    an annotated layout added

---
+++ pages/annotated-layout.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Card, Layout, Page } from '@shopify/polaris';
+
+
+class AnnotatedLayout extends React.Component {
+  state = {};
+
+  render() {
+    return (
+        <Page>
+          <Layout>
+            <Layout.AnnotatedSection
+              title="Default discount"
+              description="Add a product to Sample App, it will automatically be discounted."
+            >
+              <Card sectioned>
+                <div>Card</div>
+              </Card>
+            </Layout.AnnotatedSection>
+          </Layout>
+        </Page>
+      );
+  }
+}
+
+export default AnnotatedLayout;
